c441b7968887dc61f490d175a8650a787ab726a4
solmazsm <55103176+solmazsm@users.noreply.github.com>
2020-07-31 11:16:56 -0700

    Spring Boot create, read, update, and delete (CRUD)
    Spring Boot, Spring MVC, JPA
    create, read, update, and delete (CRUD)

---
+++ Randydemo/src/main/java/repository/CandidateRepository.java
@@ -0,0 +1,19 @@
+package repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import model.Candidate;
+import model.Candidates;
+
+public interface CandidateRepository extends JpaRepository<Candidate, Integer>{
+
+	
+
+
+
+
+
+	
+
+
+}
